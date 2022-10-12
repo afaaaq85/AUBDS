c1a9d0bfc07483eef3c955a145d8d59674747cfe
mughalfasih75@gmail.com <afaqatofficial@gmail.com>
2022-10-12 20:13:13 +0500

    homepage amendments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import About from './Components/About/About';
 import Services from './Components/Services/Services';
 import Home from './Components/Home/Home';
 import Contact from './Components/Contact/Contact';
 import Events from './Components/Events/Events';
 import Navbar from './Components/Navbar/Navbar';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 function App() {
 
 
@@ -15,7 +15,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path="/"> <Redirect to="/home" /> </Route>
           <Route path='/about' element={<About />} />
           <Route path='/services' element={<Services />} />
           <Route path='/events' element={<Events />} />
